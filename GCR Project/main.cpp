2725f955ee5a864c6cb60a314dd8c972bbdf44d8
조성현 <sunghyun1.cho@gmail.com>
2022-03-03 14:01:09 +0900

    File I/F 및 Unit Test 기본 코드 추가

--- GCR Project/main.cpp
+++ GCR Project/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+#include "File.h"
 using namespace std;
 
-int main() {
+int main(int argc, char* argv[]) {
+	string input_filename = argv[1];
+	string output_filename = argv[2];
+	File file = File();
+
 	return 0;
 }
