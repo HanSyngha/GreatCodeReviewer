2b541b937ccf7c17f05be0e2a2715fd6fe153d0e
Hyeonmin <hmin0319@gmail.com>
2022-03-03 14:32:10 +0900

    함수 구현부 원형 추가

--- GCR Project/EmployeeManager.cpp
+++ GCR Project/EmployeeManager.cpp
@@ -1,1 +1,94 @@
 #include "EmployeeManager.h"
+
+//[Add]
+void EmployeeManager::Add(const Employee& employee) {
+
+}
+
+//[Modify]
+std::map<int, Employee> EmployeeManager::ModifyByFirstName(std::string column, std::string value, std::string tagetValue) {
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::ModifyByLastName(std::string column, std::string value, std::string tagetValue) {
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::ModifyByPhoneMidNumber(std::string column, std::string value, std::string tagetValue) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::ModifyByPhoneLastNumber(std::string column, std::string value, std::string tagetValue) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::ModifyByBirthYear(std::string column, std::string value, std::string tagetValue) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::ModifyByBirthMonth(std::string column, std::string value, std::string tagetValue) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::ModifyByBirthDay(std::string column, std::string value, std::string tagetValue) {
+
+	return std::map<int, Employee>();
+}
+
+//[Delelte]
+std::map<int, Employee> EmployeeManager::DeleteByFirstName(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::DeleteByLastName(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::DeleteByPhoneMidNumber(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::DeleteByPhoneLastNumber(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::DeleteByBirthYear(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::DeleteByBirthMonth(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::DeleteByBirthDay(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+
+//[Search]
+std::map<int, Employee> EmployeeManager::SearchByFirstName(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::SearchByLastName(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::SearchByPhoneMidNumber(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::SearchByPhoneLastNumber(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::SearchByBirthYear(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::SearchByBirthMonth(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
+std::map<int, Employee> EmployeeManager::SearchByBirthDay(std::string column, std::string value) {
+
+	return std::map<int, Employee>();
+}
