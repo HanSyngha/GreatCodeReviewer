e63e20dae6479bd8c3583307cc15d0628101dc3a
Dowon Park <dowon.park@gmail.com>
2022-03-03 11:24:56 +0900

    [Feature] Add Header file for Employee / EmployManager
    Employee와 EmployeeManager 기본 interface입니다
    
    Signed-off-by: Dowon Park <dowon.park@gmail.com>

--- GCR Project/Employee.h
+++ GCR Project/Employee.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+struct Employee {
+	std::string EmpNo;
+	std::string Name;
+	std::string Career_level;
+	std::string Phone_number;
+	std::string PhoneMid;
+	std::string PhoneLast;
+	std::string BirthYear;
+	std::string BirthMonth;
+	std::string BirthDay;
+	std::string Certi;
+
+	static int makeKeyValueFromString(const std::string& employeeNum) {
+		if (employeeNum.front() == '6' || employeeNum.front() == '7' || employeeNum.front() == '8' || employeeNum.front() == '9') {
+			return(std::stoi("19" + employeeNum));
+		}
+
+		return std::stoi("20" + employeeNum);
+	}
+};
--- GCR Project/EmployeeMananger.h
+++ GCR Project/EmployeeMananger.h
@@ -1,1 +1,47 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <map>
+#include "Employee.h"
+
+
+class EmployManager {
+public:
+	//[Add]
+	void Add(const Employee& employee);
+
+	//[Modify]
+	std::map<int, Employee> ModifyByFirstName(std::string column, std::string value, std::string tagetValue);
+	std::map<int, Employee> ModifyByLastName(std::string column, std::string value, std::string tagetValue);
+	std::map<int, Employee> ModifyByPhoneMidNumber(std::string column, std::string value, std::string tagetValue);
+	std::map<int, Employee> ModifyByPhoneLastNumber(std::string column, std::string value, std::string tagetValue);
+	std::map<int, Employee> ModifyByBirthYear(std::string column, std::string value, std::string tagetValue);
+	std::map<int, Employee> ModifyByBirthMonth(std::string column, std::string value, std::string tagetValue);
+	std::map<int, Employee> ModifyByBirthDay(std::string column, std::string value, std::string tagetValue);
+
+	//[Delelte]
+	std::map<int, Employee> DeleteByFirstName(std::string column, std::string value);
+	std::map<int, Employee> DeleteByLastName(std::string column, std::string value);
+	std::map<int, Employee> DeleteByPhoneMidNumber(std::string column, std::string value);
+	std::map<int, Employee> DeleteByPhoneLastNumber(std::string column, std::string value);
+	std::map<int, Employee> DeleteByBirthYear(std::string column, std::string value);
+	std::map<int, Employee> DeleteByBirthMonth(std::string column, std::string value);
+	std::map<int, Employee> DeleteByBirthDay(std::string column, std::string value);
+
+	//[Search]
+	std::map<int, Employee> SearchByFirstName(std::string column, std::string value);
+	std::map<int, Employee> SearchByLastName(std::string column, std::string value);
+	std::map<int, Employee> SearchByPhoneMidNumber(std::string column, std::string value);
+	std::map<int, Employee> SearchByPhoneLastNumber(std::string column, std::string value);
+	std::map<int, Employee> SearchByBirthYear(std::string column, std::string value);
+	std::map<int, Employee> SearchByBirthMonth(std::string column, std::string value);
+	std::map<int, Employee> SearchByBirthDay(std::string column, std::string value);
+
+
+private:
+	void clearResults() { m_Results.clear(); };
+	std::map<int, Employee> search(std::string column, std::string value);
+
+	std::map<int, Employee> m_Employees;
+	std::map<int, Employee> m_Results;
+};
