cdde1ba6575097d1143b993610caad995e66fc8f
Dowon Park <dowon.park@gmail.com>
2022-03-07 17:19:45 +0900

    Refactoring EmployeeManager (search result 값복사 방지)
    Signed-off-by: Dowon Park <dowon.park@gmail.com>

--- UnitTest/EmployeeManager_unittest_hm.cpp
+++ UnitTest/EmployeeManager_unittest_hm.cpp
@@ -36,25 +36,26 @@ void initEmployManager(EmployeeManager& employeeManager) {
 
 TEST(EmployeeManagerHMTest, AddTest) {
 	EmployeeManager employeeManager;
-	map<int, Employee> results;
+	map<int, Employee> empty;
+	map<int, Employee>* results;
 	Employee employee;
 	employee.init("13009524", "Hyeonmin Seo", "CL3", "010-8463-5536", "19870319", "PRO");
 	AddOption option = AddOption(employee);
-	employeeManager.execute(&results, &option);
+	employeeManager.execute(&empty, &option);
 
 	SchOption option2 = SchOption("employeeNum", "13009524");
 	results = employeeManager.search(&option2);
-	EXPECT_EQ(1, results.size());
-	results.clear();
+	EXPECT_EQ(1, (*results).size());
+	(*results).clear();
 	
 	employee.init("13009521", "Hyeonmib Eeo", "CL2", "010-8463-5526", "19890130", "EXP");
 	option = AddOption(employee);
-	employeeManager.execute(&results, &option);
+	employeeManager.execute(results, &option);
 	
 	option2.fillSearchData("13009521");
 	results = employeeManager.search(&option2);
-	EXPECT_EQ(1, results.size());
-	results.clear();	
+	EXPECT_EQ(1, (*results).size());
+	(*results).clear();
 }
 
 
@@ -63,17 +64,17 @@ TEST(EmployeeManagerHMTest, ModifyByFirstNameTest)
 	EmployeeManager employeeManager;
 	initEmployManager(employeeManager);
 
-	std::map<int, Employee> result;
+	std::map<int, Employee>* result;
 
 	ModOption modOption = ModOption("name", "Hyeonmin", "phoneNum", "010-8463-5516", "-f");
 	result = employeeManager.search(&modOption);
-	employeeManager.execute(&result, &modOption);
-	result.clear();
+	employeeManager.execute(result, &modOption);
+	(*result).clear();
 
 	SchOption schOption = SchOption("phoneNum", "5516", "-l");
 	result = employeeManager.search(&schOption);
 
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, (*result).size());
 }
 
 TEST(EmployeeManagerHMTest, ModifyByLastNameTest)
@@ -81,17 +82,17 @@ TEST(EmployeeManagerHMTest, ModifyByLastNameTest)
 	EmployeeManager employeeManager;
 	initEmployManager(employeeManager);
 
-	std::map<int, Employee> result;
+	std::map<int, Employee>* result;
 
 	ModOption modOption = ModOption("name", "Seo", "cl", "CL4", "-l");
 	result = employeeManager.search(&modOption);
-	employeeManager.execute(&result, &modOption);
-	result.clear();
+	employeeManager.execute(result, &modOption);
+	(*result).clear();
 
 	SchOption schOption = SchOption("cl", "CL4");
 	result = employeeManager.search(&schOption);
 
-	EXPECT_EQ(3, result.size());
+	EXPECT_EQ(3, (*result).size());
 }
 
 TEST(EmployeeManagerHMTest, ModifyByPhoneMidNumberTest)
@@ -99,17 +100,17 @@ TEST(EmployeeManagerHMTest, ModifyByPhoneMidNumberTest)
 	EmployeeManager employeeManager;
 	initEmployManager(employeeManager);
 
-	std::map<int, Employee> result;
+	std::map<int, Employee>* result;
 
 	ModOption modOption = ModOption("phoneNum", "8443", "birthday", "19691130", "-m");
 	result = employeeManager.search(&modOption);
-	employeeManager.execute(&result, &modOption);
-	result.clear();
+	employeeManager.execute(result, &modOption);
+	(*result).clear();
 
 	SchOption schOption = SchOption("birthday", "30", "-d");
 	result = employeeManager.search(&schOption);
 
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, (*result).size());
 }
 
 TEST(EmployeeManagerHMTest, ModifyByPhoneLastNumberTest)
@@ -117,17 +118,17 @@ TEST(EmployeeManagerHMTest, ModifyByPhoneLastNumberTest)
 	EmployeeManager employeeManager;
 	initEmployManager(employeeManager);
 
-	std::map<int, Employee> result;
+	std::map<int, Employee>* result;
 
 	ModOption modOption = ModOption("phoneNum", "5516", "birthday", "19900420", "-l");
 	result = employeeManager.search(&modOption);
-	employeeManager.execute(&result, &modOption);
-	result.clear();
+	employeeManager.execute(result, &modOption);
+	(*result).clear();
 
 	SchOption schOption = SchOption("birthday", "1990", "-y");
 	result = employeeManager.search(&schOption);
 
-	EXPECT_EQ(1, result.size());
+	EXPECT_EQ(1, (*result).size());
 }
 
 TEST(EmployeeManagerHMTest, ModifyByBirthYearTest)
@@ -135,33 +136,33 @@ TEST(EmployeeManagerHMTest, ModifyByBirthYearTest)
 	EmployeeManager employeeManager;
 	initEmployManager(employeeManager);
 
-	std::map<int, Employee> result;
+	std::map<int, Employee>* result;
 
 	ModOption modOption = ModOption("birthday", "1987", "birthday", "19970402", "-y");
 	result = employeeManager.search(&modOption);
-	employeeManager.execute(&result, &modOption);
-	result.clear();
+	employeeManager.execute(result, &modOption);
+	(*result).clear();
 
 	SchOption schOption = SchOption("birthday", "04", "-m");
 	result = employeeManager.search(&schOption);
 
-	EXPECT_EQ(1, result.size());
+	EXPECT_EQ(1, (*result).size());
 }
 TEST(EmployeeManagerHMTest, ModifyByBirthMonthTest)
 {
 	EmployeeManager employeeManager;
 	initEmployManager(employeeManager);
 
-	std::map<int, Employee> result;
+	std::map<int, Employee>* result;
 
 	ModOption modOption = ModOption("birthday", "11", "birthday", "19770819", "-m");
 	result = employeeManager.search(&modOption);
-	employeeManager.execute(&result, &modOption);
-	result.clear();
+	employeeManager.execute(result, &modOption);
+	(*result).clear();
 
 	SchOption schOption = SchOption("birthday", "19", "-d");
 	result = employeeManager.search(&schOption);
-	EXPECT_EQ(3, result.size());
+	EXPECT_EQ(3, (*result).size());
 }
 
 TEST(EmployeeManagerHMTest, ModifyByBirthDayTest)
@@ -169,15 +170,15 @@ TEST(EmployeeManagerHMTest, ModifyByBirthDayTest)
 	EmployeeManager employeeManager;
 	initEmployManager(employeeManager);
 
-	std::map<int, Employee> result;
+	std::map<int, Employee>* result;
 
 	ModOption modOption = ModOption("birthday", "19", "name", "Hyeonmin Koe", "-d");
 	result = employeeManager.search(&modOption);
-	employeeManager.execute(&result, &modOption);
-	result.clear();
+	employeeManager.execute(result, &modOption);
+	(*result).clear();
 
 	SchOption schOption = SchOption("name", "Hyeonmin", "-f");
 	result = employeeManager.search(&schOption);
 
-	EXPECT_EQ(2, result.size());
+	EXPECT_EQ(2, (*result).size());
 }
